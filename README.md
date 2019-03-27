64955db521555e80bef3d77f50731d4731897dd2
Abdelghafour <abdodriowya@gmail.com>
2019-03-27 17:49:23 +0100

    correct travis config file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Devops_project
+[![Build Status](https://travis-ci.com/Th3CracKed/Devops_project.svg?token=Ua5Bde4zpdwh2oEzqAWq&branch=master)](https://travis-ci.com/Th3CracKed/Devops_project)
--- src/main/java/travis_example/Example.java
+++ src/main/java/travis_example/Example.java
@@ -1,7 +1,10 @@
 package travis_example;
 
 public class Example {
-
+    /**
+     * A simple Example to verify java doc
+     * @return true
+     */
     public boolean doStuff(){
         return true;
     }
