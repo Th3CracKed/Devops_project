84b0641572f1dd89c0b8f9ad9b550019f412a38b
Younes <younes.bensitel@etu.univ-grenoble-alpes.fr>
2019-04-07 21:40:40 +0200

    Ajout fichier csv de test
    Ajout de fichier csv test et quelques attributs dans le parser CSV.

--- src/main/java/utils/CsvParser.java
+++ src/main/java/utils/CsvParser.java
@@ -1,8 +1,25 @@
 package utils;
 
-public class CsvParser {
+import data_structure.DataFrame;
 
-    public CsvParser(String file){
-        
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+
+/* ex
+      A B C
+1 Test1 2 2
+2 Test2 5 5
+3 test3 6 6
+ */
+
+public class CsvParser {
+    private String[] mIndexs; //= new String[]{"1", "2", "3"};
+    private String[] mLabels; // = new String[]{"A", "B", "C"};
+
+    public static DataFrame CsvParser(String filename){
+
+        DataFrame res = null;
+        //DataFrame res = new DataFrame(mIndexs, mLabels, Arrays.asList("Test1","Test2","test3"),Arrays.asList(2,5,6),Arrays.asList("2","6"));
+        return res;
     }
 }
