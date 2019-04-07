f07f5b6cc6dd37d19f3f5ac66e98955e43732a8a
Younes <younes.bensitel@etu.univ-grenoble-alpes.fr>
2019-04-08 00:26:56 +0200

    Suite du parser + ajout tests

--- src/main/java/utils/CsvParser.java
+++ src/main/java/utils/CsvParser.java
@@ -3,7 +3,13 @@ package utils;
 import data_structure.DataFrame;
 
 import javax.xml.crypto.Data;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /* ex
       A B C
@@ -13,11 +19,21 @@ import java.util.Arrays;
  */
 
 public class CsvParser {
-    private String[] mIndexs; //= new String[]{"1", "2", "3"};
-    private String[] mLabels; // = new String[]{"A", "B", "C"};
+    private static String[] mIndexs; //= new String[]{"1", "2", "3"};
+    private static String[] mLabels; // = new String[]{"A", "B", "C"};
 
-    public static DataFrame CsvParser(String filename){
+    public static DataFrame parse(String filename) throws IOException {
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(";");
+                records.add(Arrays.asList(values));
+            }
+        }
+        System.out.println(records);
 
+        mLabels= records.get(0).toArray(new String[0]);
         DataFrame res = null;
         //DataFrame res = new DataFrame(mIndexs, mLabels, Arrays.asList("Test1","Test2","test3"),Arrays.asList(2,5,6),Arrays.asList("2","6"));
         return res;
